f271183a8203903335b1b8a65f425189ea62c761
Mao <lcdsmao@gmail.com>
2020-11-27 15:01:19 +0900

    Add listRow constructed from HeaderData

--- lounge-paging2/src/main/java/com/cllive/lounge/paging2/PagedListRowModel.kt
+++ lounge-paging2/src/main/java/com/cllive/lounge/paging2/PagedListRowModel.kt
@@ -2,6 +2,7 @@ package com.cllive.lounge.paging2
 
 import androidx.leanback.widget.ListRowPresenter
 import androidx.paging.PagedList
+import com.cllive.lounge.HeaderData
 import com.cllive.lounge.ListRowModel
 import com.cllive.lounge.LoungeBuildModelScope
 import com.cllive.lounge.LoungeModel
@@ -9,34 +10,40 @@ import com.cllive.lounge.memorizedController
 import com.cllive.lounge.toLoungeModelKey
 
 fun <T> LoungeBuildModelScope.pagedListRow(
-  name: String? = null,
+  headerData: HeaderData? = null,
   pagedList: PagedList<T>?,
   key: Any? = null,
   controller: PagedListLoungeController<T>,
   presenter: ListRowPresenter = ListRowModel.defaultListRowPresenter,
 ) {
-  requireKeyOrNameNonNull(key, name)
-  val keyLong: Long = key?.toLoungeModelKey() ?: name.toLoungeModelKey()
-  +ListRowModel(keyLong, name, controller, presenter)
+  requireNotNull(key ?: headerData) {
+    "Require key or headerData to be non-null."
+  }
+  val keyLong: Long = key?.toLoungeModelKey() ?: headerData.toLoungeModelKey()
+  +ListRowModel(keyLong, headerData, controller, presenter)
   controller.pagedList = pagedList
 }
 
-fun <T> LoungeBuildModelScope.pagedListRowFor(
-  name: String? = null,
+fun <T> LoungeBuildModelScope.pagedListRowOf(
+  headerData: HeaderData? = null,
   pagedList: PagedList<T>?,
   key: Any? = null,
   presenter: ListRowPresenter = ListRowModel.defaultListRowPresenter,
-  buildItemModel: (T) -> LoungeModel,
+  buildItemModel: (T?) -> LoungeModel,
+  buildModels: PagedListLoungeBuildModelScope.(List<LoungeModel>) -> Unit,
 ) {
-  val k = requireKeyOrNameNonNull(key, name)
-  val controller = memorizedController(k) {
+  val controllerKey = requireNotNull(key ?: headerData) {
+    "Require key or headerData to be non-null."
+  }
+  val controller = memorizedController(controllerKey) {
     LambdaPagedListLoungeController<T>(it)
   }
   controller.buildItemModel = { _, item ->
-    buildItemModel(requireNotNull(item) { "Item is nullable, use TODO instead." })
+    buildItemModel(item)
   }
+  controller.buildModels = buildModels
   pagedListRow(
-    name = name,
+    headerData = headerData,
     pagedList = pagedList,
     key = key,
     controller = controller,
@@ -49,26 +56,48 @@ fun <T> LoungeBuildModelScope.pagedListRowOf(
   pagedList: PagedList<T>?,
   key: Any? = null,
   presenter: ListRowPresenter = ListRowModel.defaultListRowPresenter,
-  buildItemModel: (T) -> LoungeModel,
+  buildItemModel: (T?) -> LoungeModel,
   buildModels: PagedListLoungeBuildModelScope.(List<LoungeModel>) -> Unit,
 ) {
-  val k = requireKeyOrNameNonNull(key, name)
-  val controller = memorizedController(k) {
-    LambdaPagedListLoungeController<T>(it)
-  }
-  controller.buildItemModel = { _, item ->
-    buildItemModel(requireNotNull(item) { "Item is nullable, use TODO instead" })
-  }
-  controller.buildModels = buildModels
-  pagedListRow(
-    name = name,
+  pagedListRowOf(
+    headerData = name?.let { HeaderData(it) },
     pagedList = pagedList,
     key = key,
-    controller = controller,
-    presenter = presenter
+    presenter = presenter,
+    buildItemModel = buildItemModel,
+    buildModels = buildModels,
   )
 }
 
-private fun requireKeyOrNameNonNull(key: Any?, name: String?) = requireNotNull(key ?: name) {
-  "Require key or name non-null."
+fun <T> LoungeBuildModelScope.pagedListRowFor(
+  headerData: HeaderData? = null,
+  pagedList: PagedList<T>?,
+  key: Any? = null,
+  presenter: ListRowPresenter = ListRowModel.defaultListRowPresenter,
+  buildItemModel: (T?) -> LoungeModel,
+) {
+  pagedListRowOf(
+    headerData = headerData,
+    pagedList = pagedList,
+    key = key,
+    presenter = presenter,
+    buildItemModel = buildItemModel,
+    buildModels = { +it }
+  )
+}
+
+fun <T> LoungeBuildModelScope.pagedListRowFor(
+  name: String? = null,
+  pagedList: PagedList<T>?,
+  key: Any? = null,
+  presenter: ListRowPresenter = ListRowModel.defaultListRowPresenter,
+  buildItemModel: (T?) -> LoungeModel,
+) {
+  pagedListRowFor(
+    headerData = name?.let { HeaderData(it) },
+    pagedList = pagedList,
+    key = key,
+    presenter = presenter,
+    buildItemModel = buildItemModel,
+  )
 }
