91ff6d4df9d23341e57b30d01dd9c01d99f6773f
Mao Yufeng <lcdsmao@gmail.com>
2021-03-10 10:31:04 +0900

    Fix get pagedList models deadlock (#52)
    * Properly scope cache
    
    * Rewrite getModels to avoid deadlocks
    
    * Fix
    
    * Build cache in modelBuildingDispatcher

--- lounge-paging/src/main/java/jp/co/cyberagent/lounge/paging/internal/CacheOp.kt
+++ lounge-paging/src/main/java/jp/co/cyberagent/lounge/paging/internal/CacheOp.kt
@@ -1,13 +1,13 @@
 package jp.co.cyberagent.lounge.paging.internal
 
+import jp.co.cyberagent.lounge.LoungeModel
 import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.CompletableJob
 
 internal sealed class CacheOp {
   class Insert(val position: Int, val count: Int) : CacheOp()
   class Remove(val position: Int, val count: Int) : CacheOp()
   class Move(val fromPosition: Int, val toPosition: Int) : CacheOp()
   class Change(val position: Int, val count: Int) : CacheOp()
-  class Acquire(val ack: CompletableDeferred<CompletableJob>) : CacheOp()
+  class Get(val result: CompletableDeferred<List<LoungeModel>>) : CacheOp()
   object Clear : CacheOp()
 }
