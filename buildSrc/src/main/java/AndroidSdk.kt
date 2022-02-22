c4fded8ff3f46a1c5ab1a44c9280b6b07d36a1da
Tetsuya Masuda <ttymsd@gmail.com>
2022-02-22 11:57:32 +0900

    vup fragment

--- buildSrc/src/main/java/AndroidSdk.kt
+++ buildSrc/src/main/java/AndroidSdk.kt
@@ -1,6 +1,6 @@
 object AndroidSdk {
   const val minSdk = 19
   const val testMinSdk = 21
-  const val targetSdk = 30
-  const val compileSdk = 30
+  const val targetSdk = 31
+  const val compileSdk = 31
 }
